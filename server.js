7ffd4bd7d8deb3c12dec4037f7a9e0894e5570bb
Russ Ramirez <russ.ramirez@gmail.com>
2013-06-24 14:41:51 -0500

    Update node code

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var http = require('http')
-var port = process.env.port || 1337;
+var port = process.env.port || 80;
 http.createServer(function(req, res) {
 res.writeHead(200, { 'Content-Type': 'text/plain' });
-res.end('Hello World\n');
+res.end('Hello Azure\n');
 }).listen(port);
